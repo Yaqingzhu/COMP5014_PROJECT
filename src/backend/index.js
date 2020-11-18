d35edb05c0bb3fbe13638f011417d009d7509f05
guillaume <guillaume@codecov.io>
2020-11-18 10:26:50 -0500

    REFACTOR Fix the CI process for the new login code
    Fixed the eslint errors, tests errors and fixed the workflow code. Also added a fixture for an admin user.

--- src/backend/index.js
+++ src/backend/index.js
@@ -3,7 +3,9 @@ const session = require('express-session');
 const cors = require('cors');
 const login = require('./login');
 const mysql = require('mysql');
-const bodyParser = require("body-parser")
+const bodyParser = require('body-parser');
+
+const { createAdminUser } = require('./db_util');
 
 const app = express();
 const port = process.env.API_PORT || 11234; // Port for the API
@@ -12,27 +14,27 @@ app.use(session({
   secret: 'somesecrettoken',
   saveUninitialized: true,
   resave: false,
-  cookie: { maxAge: 20*60*1000 }  // 20 mins
+  cookie: { maxAge: 20 * 60 * 1000 } // 20 mins
 }));
 
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // use bodyParser-json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 // use bodyParser-text
-app.use(bodyParser.text({type:"txt"}))
+app.use(bodyParser.text({ type: 'txt' }));
 app.use(cors({
   origin: ['http://localhost:8080', 'http://localhost:8081']
 }));
 
 app.get('/', (req, res) => {
-  res.send('Heasdsadllo World!');
+  res.send('Hello World!');
 });
 
 app.post('/login', login.loginRequestProcess);
 
-
+createAdminUser();
 
 if (process.env.NODE_ENV !== 'test') {
   app.listen(port, () => {
--- src/backend/login.js
+++ src/backend/login.js
@@ -2,27 +2,27 @@ const app = require('./index');
 const session = require('express-session');
 const mysql = require('./db_util');
 
-let connection = mysql.getDBConnection();
-
-connection.connect();
+const connection = mysql.getDBConnection();
 
 async function loginRequestProcess(req, res) {
     await checkDB(req.body.id, req.body.password, req, res);
 }
 
 function checkDB(id, password, req, res) {
+  connection.connect();
     connection.query('SELECT 1 AS result FROM login WHERE id = ? AND password = ? AND failed_time < 4', [
         id, password
-    ], (error, results) => { 
-        setLoginStatus(error, results, req, res, id)
+    ], (error, results) => {
+        setLoginStatus(error, results, req, res, id);
     });
+  connection.end();
 }
 
 function setLoginStatus(error, results, req, res, id) {
     if (error) throw error;
     const rest = results[0] ? results[0].result : -1;
 
-    if (rest == 1) {
+    if (rest === 1) {
         if (req.session && !req.session.isLogin) {
            req.session.isLogin = true;
            setUserRole(req.session, id, res);
@@ -32,40 +32,43 @@ function setLoginStatus(error, results, req, res, id) {
     }
 }
 
-function setErrorMessage(res, id){
-    new Promise((resolve, reject)=>{
-        mysql.updateFailedTimes(resolve, id)
-    }).catch((err)=>{
+function setErrorMessage(res, id) {
+    new Promise((resolve, reject) => {
+        mysql.updateFailedTimes(resolve, id);
+    }).catch(err => {
         console.log(err);
-        res.status(500).json({ loginStatus: -5,
+        res.status(500).json({
+ loginStatus: -5,
             message: 'login failed due to server issue',
             loginRole: 'none'
-        })
-    }).then((result)=>{
-        if(result){
-            if(result > 4){
-                res.status(403).json({ loginStatus: -2,
+        });
+    }).then(result => {
+        if (result) {
+            if (result > 4) {
+                res.status(403).json({
+ loginStatus: -2,
                     message: 'login failed. You tried more than 4 times and your account is locked. Please talk to admin to unlock your account.',
                     loginRole: 'none'
-                })
-            }else{
-                res.status(403).json({ loginStatus: -1,
+                });
+            } else {
+                res.status(403).json({
+ loginStatus: -1,
                     message: 'login failed. Please check your password and user id.',
                     loginRole: 'none'
-                })
+                });
             }
         }
-    });     
+    });
 }
 
-function setUserRole(session, id, res){
-    new Promise((resolve, reject)=>{
-        mysql.checkUserRole(resolve, id)
-    }).then((result)=>{
+function setUserRole(session, id, res) {
+    new Promise((resolve, reject) => {
+        mysql.checkUserRole(resolve, id);
+    }).then(result => {
         console.log('role');
         console.log(result);
         if (!session || !session.role) {
-            switch(result){
+            switch (result) {
                 case 1:
                     session.role = 'admin';
                     break;
@@ -80,13 +83,12 @@ function setUserRole(session, id, res){
             }
          }
 
-         res.status(200).json({ loginStatus: 0,
+         res.status(200).json({
+ loginStatus: 0,
                     message: 'none',
                     loginRole: result
-        })
-    })
-    
+        });
+    });
 }
 
-
-module.exports = { loginRequestProcess }
+module.exports = { loginRequestProcess };
