ca3288ac54cb6b36321c21be0dcba9add3f633c7
Yaqingzhu <yaqingzhu@cmail.carleton.ca>
2020-11-19 14:18:56 -0500

    REFACTOR update code to fix formatting

--- src/backend/admin_activities.js
+++ src/backend/admin_activities.js
@@ -27,7 +27,7 @@ function doProcess(body, req, res) {
             errorMessage: err.message,
         });
     }).then(doTimeSlotProcess(body, req, res)
-     );
+    );
 }
 
 function doTimeSlotProcess(body, req, res) {
@@ -77,8 +77,12 @@ function endRequestWithFinished(res, body) {
             errorMessage: '',
             coursePayload: result.result
         });
+    }).then(function (result) {
+    res.status(200).json({
+      responseCode: 0,
+      errorMessage: '',
+      coursePayload: result.result
     });
-    
 }
 
 module.exports = { CourseProcess };
