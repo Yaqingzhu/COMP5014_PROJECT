c8b30729285741385d9c3374559ec2c77c20e1ee
101136016GE <71817299+101136016GE@users.noreply.github.com>
2020-11-25 23:52:28 +0100

    TEST: add auxiliary functions

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,30 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+Cypress.Commands.add('log_in', (username, password) => {
+    cy.visit('http://localhost:8081/');
+    cy.server();
+    cy.route({
+        method: 'POST',
+        url: 'http://localhost:8081/login',
+        response: 200
+    }).as('login');
+    cy.visit('http://localhost:8081/login');
+    cy.get('input[type=text]').type(username).invoke('val').should((text) => {
+        expect(text).to.eq(username.toString());
+    });
+    cy.get('input[type=password]').type(password).invoke('val').should((text) => {
+        expect(text).to.eq(password);
+    });
+    cy.get('.mt-4').click();
+})
+
+Cypress.Commands.add('log_out', () => {
+    cy.get('.navbar-nav').should('be.visible').and('have.text', 'Sign out').and('not.be.disabled').click();
+    cy.url().should('eq', 'http://localhost:8081/login#')
+})
+
+Cypress.Commands.add('click_button', (name, tag) => {
+    cy.get(tag).should('have.text', name).and('not.be.disabled').click();
+    cy.wait(200);
+})
