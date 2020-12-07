3bd6e839cb5724e69d37fbb612a54342f97a2b06
101136016GE <71817299+101136016GE@users.noreply.github.com>
2020-12-07 16:56:03 +0100

    TEST: add some commands for registering

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,6 +23,8 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+import 'cypress-file-upload';
+
 Cypress.Commands.add('log_in', (username, password) => {
     cy.visit('http://localhost:8081/');
     cy.server();
@@ -48,7 +50,7 @@ Cypress.Commands.add('log_out', () => {
 
 Cypress.Commands.add('click_button', (name, tag) => {
     cy.get(tag).should('have.text', name).and('not.be.disabled').click();
-    cy.wait(200);
+    cy.wait(300);
 });
 
 Cypress.Commands.add('create_course', (name, capacity) => {
@@ -65,4 +67,64 @@ Cypress.Commands.add('create_course', (name, capacity) => {
     cy.click_button('Courses', ':nth-child(2) > .nav-link');
     cy.location('pathname').should('contain', 'courses');
     cy.contains('.h2', 'Available courses').should('be.visible');
+});
+
+Cypress.Commands.add('create_prof', (name, password) => {
+    cy.log_in(1, 'admin');
+    cy.click_button('Profs', ':nth-child(4)>.nav-link');
+    cy.click_button('New prof', '.btn');
+    cy.get('[data-testid=name]').type(name);
+    cy.get('[data-testid=password]').type(password);
+    cy.click_button('Save changes', '.btn-primary');
+});
+
+Cypress.Commands.add('assign_prof', (course, capacity, prof) => {
+    cy.log_in(1, 'admin');
+    cy.click_button('Courses', ':nth-child(2)>.nav-link');
+    cy.click_button('New course', '.btn');
+
+    cy.get('[data-testid=name]').type(course);
+    cy.get('[data-testid=capacity]').type(capacity);
+    cy.get('[data-testid=assignedProf]').select(prof);
+
+    cy.click_button('Save changes', '.btn-primary');
+});
+
+Cypress.Commands.add('create_deliverable', (password, deliverable, date) => {
+    cy.log_in(1, 'admin');
+    cy.click_button('Profs', ':nth-child(4) > .nav-link');
+    cy.get('tr').its('length').then($length => {
+        const rows = $length - 1;
+        cy.get(`tbody > :nth-child(${rows}) > :nth-child(1)`).each($elem => {
+            const prof = $elem.text();
+            cy.log_in(prof, password);
+            cy.click_button('My courses', ':nth-child(2) > .nav-link');
+
+            cy.get('tr').its('length').then($length => {
+                const rows = $length - 1;
+                cy.get(`tbody > :nth-child(${rows}) > :nth-child(2)`).each($elem => {
+                    const course = $elem.text();
+                    cy.click_button('Edit', `tbody > :nth-child(${rows}) > :nth-child(3) > a:nth-child(1)`);
+                    cy.get(':nth-child(5) > .card-body > :nth-child(2) > form > .mb-3 > [data-testid=type]').should('have.attr', 'placeholder', 'Some type').and('not.be.disabled').type(deliverable);
+                    cy.get(':nth-child(5) > .card-body > :nth-child(2) > form > .mb-3 > .react-datepicker-wrapper > .react-datepicker__input-container > .form-control').should('not.be.disabled').type('{selectall}'+date);
+                    cy.click_button('Save changes', ':nth-child(5) > .card-body > :nth-child(2) > form > .btn');
+                });
+            });
+        });
+    });
+});
+
+Cypress.Commands.add('register_course', () => {
+    cy.log_in(223, 'test');
+    cy.click_button('My courses', ':nth-child(2) > .nav-link');
+
+    cy.click_button('Register to a new course', '.btn');
+    cy.get('tr').its('length').then($length => {
+        const rows = $length - 1;
+        cy.get(`tbody > :nth-child(${rows}) > :nth-child(1)`).each($elem => {
+            const id = $elem.text();
+            cy.get(`[data-testid=check-${id}]`).not('[disabled]').check();
+            cy.click_button('Register', '.btn');
+        });
+    });
 });
